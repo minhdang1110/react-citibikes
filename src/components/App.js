3e2104eb18e91a23844378f2c109fc7820c7cf35
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 03:35:02 -0400

    fix landingp pageimport

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "../pages/LandingPage";
 import '../../node_modules/materialize-css/dist/css/materialize.min.css';
 
 class App extends React.Component{
