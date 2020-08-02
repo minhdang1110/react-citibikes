8b674a3f4cfd57a132a5ac08e0e79e85ca943744
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 02:46:48 -0400

    add getavailable cities

--- src/utils/getAvailableCountryandCity.js
+++ src/utils/getAvailableCountryandCity.js
@@ -5,3 +5,15 @@ const getNetworkData = async () => {
     const data = await axios.get('http://api.citybik.es/v2/networks');
     return data.data.networks;
 }
+
+export const getAvailableCities = async () => {
+    const network = await getNetworkData();
+
+    const cities = _.map(network, (data) => {
+        return data.location.city;
+    })
+
+    const ucities = _.uniq(cities)
+    console.log('ucities', ucities)
+    return ucities;
+}
