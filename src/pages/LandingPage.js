d25fb63cc7ce3db52fa6b111e8a75f816ef8b5d5
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 02:10:26 -0400

    create input fields and search button in landingpage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,16 +1,22 @@
 import React from "react";
-import '../styles/LandingPage.css'
+import "../styles/LandingPage.css";
 
 class LandingPage extends React.Component {
     render() {
         return (
             <div className="container">
                 <h3>Welcome to FindACityBike</h3>
+                
                 <p className="inform-paragraph">
                     Please turn on your Wifi and input the city where you want to find a
                     nearest citybike station
                 </p>
-                This is a landing page
+                
+                <div className="dashboard-input">
+                    <input type="text" placeholder="City Name. Ex: New York" />
+                </div>
+         
+                <button className="dashboard-search-button"> <i class="prefix material-icons">search</i>Search</button>
             </div>
         );
     }
