f2b04407cd8ba7e022d40625518181aac371b646
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 03:09:35 -0400

    add validate function

--- src/components/FindCityandCountry.js
+++ src/components/FindCityandCountry.js
@@ -1,3 +1,9 @@
 import Validate from "../Validator";
 import {getAvailableCountries,getAvailableCities,} from "../utils/getAvailableCountryandCityFromCityBike";
 import React, { useState } from "react";
+
+function ValidateList() {
+    let [display, setDisplay] = useState(false);
+};
+
+export default ValidateList;
