a36dabbd4b5bc1905123d38ed1993a72089b9de0
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 02:01:36 -0400

    updaate landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,18 @@
 import React from "react";
+import '../styles/LandingPage.css'
 
 class LandingPage extends React.Component {
     render() {
-        return <div>This is a landing page</div>;
+        return (
+            <div className="container">
+                <h3>Welcome to FindACityBike</h3>
+                <p className="inform-paragraph">
+                    Please turn on your Wifi and input the city where you want to find a
+                    nearest citybike station
+                </p>
+                This is a landing page
+            </div>
+        );
     }
 }
 
