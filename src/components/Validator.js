d12b05c097fd3c149fe5e61bfada6bb1b72ae2c2
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 02:56:31 -0400

    add state

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -1,2 +1,11 @@
 import React from "react";
 import _ from 'lodash';
+
+class Validator extends React.Component {
+    state = {
+        choice: "",
+        isAvailable : false
+    };
+}
+
+export default Validator;
