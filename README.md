b9f588b19718dc18379c9641de7fab98e40b333a
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 02:15:47 -0400

    add comment

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Having multiple filters (name, system, company, etc.)
 ## Wireframe
 ![Wireframe](wireframe.png)
 
-This project was bsootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -12,10 +12,12 @@ class LandingPage extends React.Component {
                     nearest citybike station
                 </p>
                 
+                {/* Input field */}
                 <div className="dashboard-input">
                     <input type="text" placeholder="City Name. Ex: New York" />
                 </div>
          
+                {/* Search button */}
                 <button className="dashboard-search-button"> <i class="prefix material-icons">search</i>Search</button>
             </div>
         );
