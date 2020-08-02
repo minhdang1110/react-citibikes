d42838e7e1fe06ab28f8e45f98ad008bfd73e377
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 02:45:55 -0400

    add getNetworkData

--- src/utils/getAvailableCountryandCity.js
+++ src/utils/getAvailableCountryandCity.js
@@ -1,2 +1,7 @@
 import axios from 'axios'; 
 import _ from 'lodash';
+
+const getNetworkData = async () => {
+    const data = await axios.get('http://api.citybik.es/v2/networks');
+    return data.data.networks;
+}
