0323e0b3eff0f423862f2cd9ec8cf1b26029be20
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 23:46:30 -0400

    add station-locaiton

--- src/utils/getMatchLocation.js
+++ src/utils/getMatchLocation.js
@@ -5,3 +5,13 @@ import {getNetworkData} from './common';
 export const getStationIdThatMatchCountrySearch = (choice) => {
 
 }
+
+export const generateStationIdWithLocation = async () => {
+    const network = await getNetworkData();
+    let stationIdWithLocation = [];
+    _.map(network, ({id, location}) => {
+        stationIdWithLocation.push({id, location})
+    })
+    // console.log(network);
+    return stationIdWithLocation;
+}
