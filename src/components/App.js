e5a598a1203dc89e4956edcba593f94a5290d2c6
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-03 15:00:35 -0400

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import LandingPage from "../pages/LandingPage";
+import SpecificStation from "./SpecificStation";
+
 import '../../node_modules/materialize-css/dist/css/materialize.min.css';
+
 
 class App extends React.Component{
   render(){
     return(
       <div>
         <BrowserRouter>
-          <Route path="/" component={LandingPage}/>
+          <Switch>
+            <Route path="/" exact component={LandingPage} />
+            <Route path="/station/:id" component={SpecificStation} />
+          </Switch>
         </BrowserRouter>
       </div>
-    )
+    );
   }
 }
 
