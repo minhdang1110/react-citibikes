03dc4a198162624e53ab2f1c5194b2bf49399998
minhdang1110 <nhatminhdang04@gmail.com>
2020-08-02 23:50:14 -0400

    import common to getAvailLocation

--- src/utils/getAvailableLocation.js
+++ src/utils/getAvailableLocation.js
@@ -1,10 +1,12 @@
 import axios from 'axios'; 
 import _ from 'lodash';
 
-const getNetworkData = async () => {
-    const data = await axios.get('http://api.citybik.es/v2/networks');
-    return data.data.networks;
-}
+import {getNetworkData} from './common';
+
+// const getNetworkData = async () => {
+//     const data = await axios.get('http://api.citybik.es/v2/networks');
+//     return data.data.networks;
+// }
 
 export const getAvailableCities = async () => {
     const network = await getNetworkData();
